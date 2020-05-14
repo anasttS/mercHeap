c35c002f0bb22f21dd426a788ed7bdb50abb41dc
Anastasia <tokod684@mail.ru>
2020-05-15 02:46:08 +0500

    add registration functionality

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -22,8 +22,10 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     {
         parent::__construct($registry, User::class);
     }
+
     /**
      * Used to upgrade (rehash) the user's password automatically over time.
+     * @throws \Doctrine\ORM\ORMException
      */
     public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
     {
