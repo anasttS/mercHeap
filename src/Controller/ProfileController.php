6ff93ebaef0d687bc80505520b0c500f86502bf1
ksush <ksushka.e@mail.ru>
2020-05-17 00:15:38 +0300

    change in profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,6 +6,8 @@ use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @IsGranted("ROLE_USER")
  */
@@ -16,13 +18,13 @@ class ProfileController extends AbstractController
      */
     public function profile(LoggerInterface $logger)
     {
-        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
+        $logger->debug('Checking account page for ' . $this->getUser()->getEmail());
         $username = $this->getUser()->getName();
         $about = $this->getUser()->getAbout();
         $merch = $this->getUser()->getProducts();
         $my_orders = $this->getUser()->getOrders();
         $links = $this->getUser()->getLinks();
-        $count = ceil(count($merch)/3);
+        $count = ceil(count($merch) / 3);
 
         return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
@@ -34,5 +36,19 @@ class ProfileController extends AbstractController
             'count' => $count
         ]);
 //        dd($this->getUser()->getUsername());
+    }
+
+    /**
+     * @Route("/profile/addImage", name="upload_image")
+     */
+    public function changeProfile(Request $request)
+    {
+        $uploadedFile = $request->files->get('image');
+        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+        dd($uploadedFile->move($destination,
+            $newFilename
+        ));
     }
 }
