4214f59176dff772a761a5bae542f99843e47444
ksush <ksushka.e@mail.ru>
2020-05-19 22:05:59 +0300

    add ORDERUSER((((

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Entity\Order;
+use App\Entity\OrderUser;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -27,7 +27,7 @@ class OrderType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => Order::class,
+            'data_class' => OrderUser::class,
         ));
     }
 }
