5e344d42c566020352f047512172b26cb91c8fba
ksush <ksushka.e@mail.ru>
2020-05-20 22:57:27 +0300

    add entity comment

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Product;
 use App\Entity\User;
 use App\Repository\ProductRepository;
+use CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -13,10 +16,15 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{id}", name="product")
      */
-    public function index($id, ProductRepository $productRepository)
+    public function index($id, ProductRepository $productRepository, Request $request)
     {
         $product = $productRepository->find($id);
         $user = $product->getUser();
+
+        $comment = new Comment();
+        $comment-> setUser($this->getUser());
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
 
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -26,9 +26,44 @@ class Comment
      */
     private $description;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
     public function getDescription(): string
     {
         return (string) $this->description;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
     }
 
 }
