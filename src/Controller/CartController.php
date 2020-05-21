ee9d708a4f1ffba25cec8f8e3ff074593579c113
Anastasia <tokod684@mail.ru>
2020-05-21 18:50:48 +0500

    cart + goods

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+
     /**
      * @Route("/cart", name="cart")
      */
     public function index()
     {
+        if ($_POST['search']) {
+            $json = $_POST['search'];
+            $array = json_decode($json);
+        } else {
+            $array = array();
+        }
+        $products = array();
+        foreach ($array as $k => $v) {
+            $product = $this->productRepository->find($k);
+            array_push($products, $product);
+        }
 
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'products' => $products,
+            'controller_name' => 'CartController'
         ]);
     }
 }
