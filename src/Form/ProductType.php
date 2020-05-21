7cb3fa678668019009169ce5add91e4747b169ef
Adeliya Nazipova <adeliyanazipova@mail.ru>
2020-05-21 14:46:37 +0300

    add product page added

---
+++ src/Form/ProductType.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('cost', TextType::class,)
+            ->add('photo', FileType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Product::class,
+        ));
+    }
+}
